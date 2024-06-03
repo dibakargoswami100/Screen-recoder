d811af47988fa25a568c950de7c5fe89f0114df6
Dibakar Goswami <160622594+dibakargoswami100@users.noreply.github.com>
2024-06-03 07:38:38 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import AllCamera from './AllCamera';
+
+function App() {
+    return (
+        <div className="App">
+            <AllCamera />
+        </div>
+    );
+}
+
+export default App;
